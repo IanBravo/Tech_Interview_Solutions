6ab37cd9355b75a73217d9ff53fe41867de9a669
Ian Bravo <ian_bravo@hotmail.com>
2018-03-16 18:07:57 -0600

    merge trees solution

--- CodeFights.java
+++ CodeFights.java
@@ -1071,3 +1071,22 @@ public ListNode swapPairs(ListNode head)
     }
     return dummy.next;
 }
+
+/////////////// merge trees
+public TreeNode mergeTrees(TreeNode t1, TreeNode t2) 
+{
+    if (t1 == null && t2 == null) return null;
+    
+    if (t1 != null && t2 == null) return t1;
+    
+    if (t1 == null && t2 != null) return t2;
+    
+    t1.val += t2.val;
+    
+    t1.left = mergeTrees(t1.left, t2.left);
+    t1.right = mergeTrees(t1.right, t2.right);
+    
+    return t1;
+}
+
+///////////////////
