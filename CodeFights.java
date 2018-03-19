4be62557820e3df86a01615ee4c2bb4ec46124c6
Ian Bravo <ian_bravo@hotmail.com>
2018-03-19 15:32:21 -0600

    Maximum Product of Three Numbers solution

--- CodeFights.java
+++ CodeFights.java
@@ -1307,4 +1307,41 @@ public int[] productExceptSelf(int[] intArray)
     return productsOfAllIntsExceptAtIndex;
 }
 
-//////////////////    
+////////////////// Maximum Product of Three Numbers
+
+public int maximumProduct(int[] arrayOfInts) 
+{
+    int highest = Math.max(arrayOfInts[0], arrayOfInts[1]);
+    int lowest  = Math.min(arrayOfInts[0], arrayOfInts[1]);
+
+    int highestProductOf2 = arrayOfInts[0] * arrayOfInts[1];
+    int lowestProductOf2  = arrayOfInts[0] * arrayOfInts[1];
+
+    int highestProductOf3 = arrayOfInts[0] * arrayOfInts[1] * arrayOfInts[2];
+
+    for (int i = 2; i < arrayOfInts.length; i++) 
+    {
+        int current = arrayOfInts[i];
+
+        highestProductOf3 = Math.max(Math.max(
+            highestProductOf3,
+            current * highestProductOf2),
+            current * lowestProductOf2);
+
+        highestProductOf2 = Math.max(Math.max(
+            highestProductOf2,
+            current * highest),
+            current * lowest);
+
+        lowestProductOf2 = Math.min(Math.min(
+            lowestProductOf2,
+            current * highest),
+            current * lowest);
+
+        highest = Math.max(highest, current);
+
+        lowest = Math.min(lowest, current);
+    }
+
+    return highestProductOf3;
+}
