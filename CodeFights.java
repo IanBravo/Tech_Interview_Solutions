beda2b1fb9abc28675412004e6eede20d95682d1
Ian Bravo <ian_bravo@hotmail.com>
2018-03-21 15:58:40 -0600

    Longest Valid Parentheses solution

--- CodeFights.java
+++ CodeFights.java
@@ -1437,3 +1437,32 @@ public String reverseWords(String s)
     
     return word.toString();
 }
+
+//////////////////// Longest Valid Parentheses
+
+public int longestValidParentheses(String s) 
+{
+    int max = 0;
+    Stack<Integer> stack = new Stack<>();
+    stack.push(-1);
+    for (int i = 0; i < s.length(); i++) 
+    {
+        if (s.charAt(i) == '(') 
+        {
+            stack.push(i);
+        } 
+        else 
+        {
+            stack.pop();
+            if (stack.empty()) 
+            {
+                stack.push(i);
+            } 
+            else 
+            {
+                max = Math.max(max, i - stack.peek());
+            }
+        }
+    }
+    return max;
+}
