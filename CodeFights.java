9f38d105ced62d44a58891763edaf9e1edc7a3ac
Ian Bravo <ian_bravo@hotmail.com>
2018-03-21 21:19:38 -0600

    First Unique Character in a String solution

--- CodeFights.java
+++ CodeFights.java
@@ -1495,3 +1495,19 @@ public boolean canConstruct(String ransomNote, String magazine)
     
     return true;
 }
+
+/////////////////// First Unique Character in a String
+
+public int firstUniqChar(String s) 
+{
+    if (s.length() == 0) return -1;
+    
+    int[] letters = new int[26];
+    for (int i = 0; i < s.length(); i++) 
+        letters[s.charAt(i) - 'a']++;
+    
+    for (int i = 0; i < s.length(); i++)
+        if (letters[s.charAt(i) - 'a'] == 1) return i;
+    
+    return -1;
+}
