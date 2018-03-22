9a524d0079e3828d66d85fcff7eb9f9b30ebb9c7
Ian Bravo <ian_bravo@hotmail.com>
2018-03-21 20:03:20 -0600

    One Edit Distance solution

--- CodeFights.java
+++ CodeFights.java
@@ -1466,3 +1466,18 @@ public int longestValidParentheses(String s)
     }
     return max;
 }
+
+/////////////////// One Edit Distance
+
+public boolean isOneEditDistance(String s, String t) 
+{
+    int m = s.length(), n = t.length();
+    if (m > n) return isOneEditDistance(t, s);
+    if (n - m > 1) return false;
+    int i = 0, shift = n - m;
+    while (i < m && s.charAt(i) == t.charAt(i)) i++;
+    if (i == m) return shift > 0;
+    if (shift == 0) i++;
+    while (i < m && s.charAt(i) == t.charAt(i + shift)) i++;
+    return i == m;
+}
