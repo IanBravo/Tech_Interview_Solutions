2224f606fa20ff510245044a809f200e0f04dcda
Ian Bravo <ian_bravo@hotmail.com>
2018-03-24 23:34:29 -0600

    3Sum (equals 0) solution

--- CodeFights.java
+++ CodeFights.java
@@ -1653,3 +1653,42 @@ class Solution
         return R - L - 1;
     }
 }
+
+/////////////////////// 3Sum (equals 0)
+
+public List<List<Integer>> threeSum(int[] nums) 
+{
+    List<List<Integer>> sums = new ArrayList<List<Integer>>();  
+    Arrays.sort(nums);
+    List<Integer> sum = new ArrayList<Integer>();
+    
+    for (int i = 0; i < nums.length - 2; i++)
+    {
+        if (i == 0 || (i > 0 && nums[i] != nums[i - 1]))
+        {
+            int j = i + 1, k = nums.length - 1, target = 0 - nums[i];
+            while (j < k)
+            {
+                if (nums[j] + nums[k] == target)
+                {
+                    sum.add(nums[i]);
+                    sum.add(nums[j]);
+                    sum.add(nums[k]);
+                    sums.add(new ArrayList<Integer>(sum));
+                    sum.clear();
+                    while(j < k && nums[j] == nums[j + 1]) j++;
+                    while(j < k && nums[k] == nums[k - 1]) k--;
+                    k--;
+                    j++;
+                }
+                else if (nums[j] + nums[k] < target)
+                    j++;
+                else
+                    k--;
+            }
+        }                                   
+    }
+    return sums;
+}
+
+//////////////////////
