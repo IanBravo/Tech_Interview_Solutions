5c61a03d70fd9d4a92a9d7e528a38a25ef3c6bf5
Ian Bravo <ian_bravo@hotmail.com>
2018-03-31 00:13:37 -0600

    Kth Largest Element in an Array solution

--- CodeFights.java
+++ CodeFights.java
@@ -1906,3 +1906,11 @@ class Solution {
         }
     }
 }
+
+/////////////////////// Kth Largest Element in an Array
+
+public int findKthLargest(int[] nums, int k) 
+{
+    Arrays.sort(nums);
+    return nums[nums.length - k];
+}
