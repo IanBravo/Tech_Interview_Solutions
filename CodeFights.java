6ebe0c1fdc72dfc7a9c6e6b3c09158f1f5c1da71
Ian Bravo <ian_bravo@hotmail.com>
2018-03-31 19:32:58 -0600

    DP: Maximal Square solution

--- CodeFights.java
+++ CodeFights.java
@@ -1955,4 +1955,23 @@ static int pathSum(int[][] grid, int row, int column, int sum)
     return sum;
 }
 
-//////////////////////////////
+////////////////////////////// DP: Maximal Square
+
+public int maximalSquare(char[][] matrix) 
+{
+    int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
+    int[][] dp = new int[rows + 1][cols + 1];
+    int maxArea = 0;
+    for (int i = 1; i <= rows; i++) 
+    {
+        for (int j = 1; j <= cols; j++) 
+        {
+            if (matrix[i - 1][j - 1] == '1')
+            {
+                dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                maxArea = Math.max(maxArea, dp[i][j]);
+            }
+        }
+    }
+    return maxArea * maxArea;
+}
