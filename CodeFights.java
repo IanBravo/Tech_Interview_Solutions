690e31b529c3520c5eadde19645ba972451f7ddc
Ian Bravo <ian_bravo@hotmail.com>
2018-03-31 20:04:41 -0600

    Longest Consecutive Sequence solution

--- CodeFights.java
+++ CodeFights.java
@@ -1975,3 +1975,31 @@ public int maximalSquare(char[][] matrix)
     }
     return maxArea * maxArea;
 }
+
+//////////////////////////// Longest Consecutive Sequence
+
+public int longestConsecutive(int[] nums) 
+{
+    if (nums.length == 0) return 0;
+    
+    int current = 1, longest = 1;
+    Arrays.sort(nums);
+    
+    for (int i = 1; i < nums.length; i++)
+    {
+        if (nums[i] != nums[i - 1])
+        {
+            if (nums[i] == nums[i - 1] + 1)
+            {
+                current++;
+            }
+            else
+            {
+                longest = Math.max(longest, current);
+                current = 1;
+            }
+        }           
+    }
+    
+    return Math.max(current, longest);
+}
