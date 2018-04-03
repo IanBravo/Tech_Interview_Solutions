cb356edcdd4cc6f195de479e741cabaa49100279
Ian Bravo <ian_bravo@hotmail.com>
2018-04-03 11:59:39 -0500

    Edit Distance solution

--- CodeFights.java
+++ CodeFights.java
@@ -2248,3 +2248,27 @@ public int numDistinct(String s, String t)
     
     return dp[s.length()][t.length()];
 }
+
+//////////////////////// Edit Distance
+
+public int minDistance(String word1, String word2) 
+{
+    int m = word1.length();
+    int n = word2.length();        
+    int[][] cost = new int[m + 1][n + 1];
+    
+    for (int i = 0; i <= m; i++) cost[i][0] = i;
+    for (int i = 1; i <= n; i++) cost[0][i] = i;
+    
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (word1.charAt(i) == word2.charAt(j))
+                cost[i + 1][j + 1] = cost[i][j];
+            else
+                cost[i + 1][j + 1] = Math.min(Math.min(cost[i][j], cost[i][j + 1]), cost[i + 1][j]) + 1;
+        }
+    }
+    return cost[m][n];
+}
