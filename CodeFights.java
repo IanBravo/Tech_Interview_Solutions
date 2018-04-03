1791377742153deb5a1dba3334d979b732dc57e6
Ian Bravo <ian_bravo@hotmail.com>
2018-04-02 19:24:56 -0500

    longest Common Subsequence solution

--- CodeFights.java
+++ CodeFights.java
@@ -2204,3 +2204,25 @@ public int uniquePathsWithObstacles(int[][] obstacleGrid)
     }
     return matrix[0][0];
 }
+
+////////////////////// longest Common Subsequence
+
+int longestCommonSubsequence(String s, String t) 
+{
+    int m = s.length();
+    int n = t.length();
+    int[][] dp = new int[m + 1][n + 1];
+    
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (s.charAt(i - 1) == t.charAt(j - 1))
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            else
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    
+    return dp[m][n];
+}
