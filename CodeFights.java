8a4d4973772507b8c2a92f480e11786e877a3395
Ian Bravo <ian_bravo@hotmail.com>
2018-04-14 21:58:48 -0500

    Spiral Matrix solution

--- CodeFights.java
+++ CodeFights.java
@@ -2384,3 +2384,33 @@ public boolean isNumber(String s)
     while (i < n && Character.isWhitespace(s.charAt(i))) i++;
     return isNumeric && i == n;
 }
+
+///////////////////// Spiral Matrix
+public List<Integer> spiralOrder(int[][] matrix) 
+{
+    List<Integer> elements = new ArrayList<>();
+    if (matrix.length == 0) return elements;
+    int m = matrix.length, n = matrix[0].length;
+    int row = 0, col = -1;
+    
+    while (true)
+    {
+        for (int i = 0; i < n; i++)
+            elements.add(matrix[row][++col]);
+        
+        if (--m == 0) break;
+        for (int i = 0; i < m; i++)
+            elements.add(matrix[++row][col]);
+        
+        if (--n == 0) break;
+        for (int i = 0; i < n; i++)
+            elements.add(matrix[row][--col]);
+        
+        if (--m == 0) break;
+        for (int i = 0; i < m; i++)
+            elements.add(matrix[--row][col]);
+        
+        if (--n == 0) break;
+    }
+    return elements;
+}
