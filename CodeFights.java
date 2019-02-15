622aa6ab9fa32704385aab2d11744f4f6356c2d3
Ian Bravo <ian_bravo@hotmail.com>
2019-02-14 19:38:50 -0600

    Closest to target

--- CodeFights.java
+++ CodeFights.java
@@ -3398,3 +3398,31 @@ public static void matrixZigZag(int[][] matrix)
             System.out.print(matrix[--i][++j] + " ");
     }
 }
+
+////// Closest to target
+////// Time: O(log(n)) Space: O(1)
+public static int closestToTarget(int[] a, int target)
+{
+    if (a == null || a.length == 0) return -1;
+    
+    int start = 0, end = a.length - 1, distance = Integer.MAX_VALUE, closest = -1;
+    
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        int temp = Math.abs(target - a[mid]);
+        if (temp < distance)
+        {
+            distance = temp;
+            closest = a[mid];
+        }
+        
+        if (a[mid] > target)
+            end = mid - 1;
+        else if (a[mid] < target)
+            start = mid + 1;
+        else
+            return mid;
+    }
+    return closest;
+}
